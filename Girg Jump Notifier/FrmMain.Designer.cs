b40e15aba450cba158b78177c3010036f2b3f47c
vurtic <vurtic.qed@gmail.com>
2016-08-23 00:21:39 -0400

    Fixed buttons being too small.

--- Girg Jump Notifier/FrmMain.Designer.cs
+++ Girg Jump Notifier/FrmMain.Designer.cs
@@ -37,21 +37,20 @@
             this.sptLabels = new System.Windows.Forms.SplitContainer();
             this.lblJump = new System.Windows.Forms.Label();
             this.lblShield = new System.Windows.Forms.Label();
-            this.pnlControls = new System.Windows.Forms.Panel();
             this.pnlControlsWrapper = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.sptLabels)).BeginInit();
             this.sptLabels.Panel1.SuspendLayout();
             this.sptLabels.Panel2.SuspendLayout();
             this.sptLabels.SuspendLayout();
-            this.pnlControls.SuspendLayout();
             this.pnlControlsWrapper.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(0, 0);
+            this.btnConnect.Location = new System.Drawing.Point(3, 1);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(90, 23);
+            this.btnConnect.Size = new System.Drawing.Size(70, 19);
             this.btnConnect.TabIndex = 1;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = true;
@@ -59,9 +58,10 @@
             // 
             // btnDisconnect
             // 
-            this.btnDisconnect.Location = new System.Drawing.Point(96, 0);
+            this.btnDisconnect.Location = new System.Drawing.Point(81, 1);
+            this.btnDisconnect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDisconnect.Name = "btnDisconnect";
-            this.btnDisconnect.Size = new System.Drawing.Size(90, 23);
+            this.btnDisconnect.Size = new System.Drawing.Size(70, 19);
             this.btnDisconnect.TabIndex = 2;
             this.btnDisconnect.Text = "Disconnect";
             this.btnDisconnect.UseVisualStyleBackColor = true;
@@ -71,14 +71,15 @@
             // 
             this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(67, 4);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // chkTop
             // 
             this.chkTop.AutoSize = true;
-            this.chkTop.Location = new System.Drawing.Point(192, 2);
+            this.chkTop.Location = new System.Drawing.Point(161, 3);
+            this.chkTop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chkTop.Name = "chkTop";
-            this.chkTop.Size = new System.Drawing.Size(78, 21);
+            this.chkTop.Size = new System.Drawing.Size(62, 17);
             this.chkTop.TabIndex = 3;
             this.chkTop.Text = "On Top";
             this.chkTop.UseVisualStyleBackColor = true;
@@ -89,7 +90,8 @@
             this.sptLabels.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.sptLabels.Location = new System.Drawing.Point(12, 41);
+            this.sptLabels.Location = new System.Drawing.Point(9, 33);
+            this.sptLabels.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.sptLabels.Name = "sptLabels";
             this.sptLabels.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -100,8 +102,9 @@
             // sptLabels.Panel2
             // 
             this.sptLabels.Panel2.Controls.Add(this.lblShield);
-            this.sptLabels.Size = new System.Drawing.Size(282, 124);
-            this.sptLabels.SplitterDistance = 60;
+            this.sptLabels.Size = new System.Drawing.Size(231, 101);
+            this.sptLabels.SplitterDistance = 48;
+            this.sptLabels.SplitterWidth = 3;
             this.sptLabels.TabIndex = 4;
             // 
             // lblJump
@@ -110,8 +113,9 @@
             this.lblJump.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblJump.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblJump.Location = new System.Drawing.Point(0, 0);
+            this.lblJump.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblJump.Name = "lblJump";
-            this.lblJump.Size = new System.Drawing.Size(282, 60);
+            this.lblJump.Size = new System.Drawing.Size(231, 48);
             this.lblJump.TabIndex = 0;
             this.lblJump.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -121,40 +125,33 @@
             this.lblShield.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblShield.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblShield.Location = new System.Drawing.Point(0, 0);
+            this.lblShield.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblShield.Name = "lblShield";
-            this.lblShield.Size = new System.Drawing.Size(282, 60);
+            this.lblShield.Size = new System.Drawing.Size(231, 50);
             this.lblShield.TabIndex = 1;
             this.lblShield.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // pnlControls
-            // 
-            this.pnlControls.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pnlControls.Controls.Add(this.chkTop);
-            this.pnlControls.Controls.Add(this.btnDisconnect);
-            this.pnlControls.Controls.Add(this.btnConnect);
-            this.pnlControls.Location = new System.Drawing.Point(6, 0);
-            this.pnlControls.Name = "pnlControls";
-            this.pnlControls.Size = new System.Drawing.Size(270, 23);
-            this.pnlControls.TabIndex = 8;
-            // 
             // pnlControlsWrapper
             // 
-            this.pnlControlsWrapper.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pnlControlsWrapper.Controls.Add(this.pnlControls);
-            this.pnlControlsWrapper.Location = new System.Drawing.Point(12, 12);
+            this.pnlControlsWrapper.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pnlControlsWrapper.Controls.Add(this.chkTop);
+            this.pnlControlsWrapper.Controls.Add(this.btnConnect);
+            this.pnlControlsWrapper.Controls.Add(this.btnDisconnect);
+            this.pnlControlsWrapper.Location = new System.Drawing.Point(9, 10);
+            this.pnlControlsWrapper.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnlControlsWrapper.Name = "pnlControlsWrapper";
-            this.pnlControlsWrapper.Size = new System.Drawing.Size(282, 23);
+            this.pnlControlsWrapper.Size = new System.Drawing.Size(231, 19);
             this.pnlControlsWrapper.TabIndex = 9;
             // 
             // FrmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(308, 179);
+            this.ClientSize = new System.Drawing.Size(250, 145);
             this.Controls.Add(this.pnlControlsWrapper);
             this.Controls.Add(this.sptLabels);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmMain";
@@ -164,9 +161,8 @@
             this.sptLabels.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.sptLabels)).EndInit();
             this.sptLabels.ResumeLayout(false);
-            this.pnlControls.ResumeLayout(false);
-            this.pnlControls.PerformLayout();
             this.pnlControlsWrapper.ResumeLayout(false);
+            this.pnlControlsWrapper.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -177,7 +173,6 @@
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.CheckBox chkTop;
         private System.Windows.Forms.SplitContainer sptLabels;
-        private System.Windows.Forms.Panel pnlControls;
         private System.Windows.Forms.Panel pnlControlsWrapper;
         private System.Windows.Forms.Label lblJump;
         private System.Windows.Forms.Label lblShield;
